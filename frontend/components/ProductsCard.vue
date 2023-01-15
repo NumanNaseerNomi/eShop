63c5baed2af2ac0445361d8dc676df17a5a3003d
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-01-16 00:28:17 +0500

    Update ProductsCard.vue

--- frontend/components/ProductsCard.vue
+++ frontend/components/ProductsCard.vue
@@ -7,17 +7,14 @@
             <svg width="100%" height="300">
               <rect width="100%" height="100%" fill="#adb5bd"></rect>
             </svg>
-            <div class="card-body">
-              <h5 class="card-title placeholder-glow">
-                <span class="placeholder col-6"></span>
-              </h5>
-              <p class="card-text placeholder-glow">
-                <span class="placeholder col-7"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-8"></span>
+            <div class="card-body placeholder-glow">
+              <h6 class="placeholder col-10"></h6>
+              <p>
+                <span class="placeholder mx-2 col-3"></span>
+                <span class="placeholder mx-2 col-3"></span>
+                <span class="placeholder mx-2 col-3"></span>
               </p>
-              <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+              <a href="#" tabindex="-1" class="btn btn-secondary disabled placeholder col-12"></a>
             </div>
           </div>
         </div>
