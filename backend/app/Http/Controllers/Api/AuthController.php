c1fa5e81cedb1925a00524f2663c444e41f14ec5
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-03-22 01:00:07 +0500

    Update AuthController.php

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,11 +13,15 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        if (auth()->attempt($request->all())) {
-            return response([
+        if(auth()->attempt($request->all()))
+        {
+            $data =
+            [
                 'user' => auth()->user(),
                 'access_token' => auth()->user()->createToken('authToken')->accessToken
-            ], Response::HTTP_OK);
+            ];
+
+            return response($data, Response::HTTP_OK);
         }
 
         return response([
@@ -33,5 +38,13 @@ class AuthController extends Controller
         ]);
 
         return response($user, Response::HTTP_CREATED);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
