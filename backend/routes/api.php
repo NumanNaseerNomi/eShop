0f8f6d2d983aeb0b041e4aab2cfd754423b1a9d5
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-03-22 01:00:11 +0500

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request;
 });
