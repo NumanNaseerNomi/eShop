2046495d417de12d3d621aba09e7e7c207636636
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-03-21 23:59:22 +0500

    Update AuthController.php

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        if (auth()->attempt($request->all())) {
+            return response([
+                'user' => auth()->user(),
+                'access_token' => auth()->user()->createToken('authToken')->accessToken
+            ], Response::HTTP_OK);
+        }
+
+        return response([
+            'message' => 'This User does not exist'
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
 }
