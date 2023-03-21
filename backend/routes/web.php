2682001f1dc4bb4c3fe1f1c9dfd25b5648537106
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-03-21 23:25:31 +0500

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,5 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return redirect(env('WEB_APP_URL') ?? 'welcome'); });
-Route::view('welcome', 'welcome');
+Route::view('/', 'welcome');
