0a259feeccc37f04a385b070839be9914f324833
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-03-23 01:48:45 +0500

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    runtimeConfig:
+    {
+        public:
+        {
+            API_URL: process.env.API_URL || 'http://localhost:8000/api',
+        }
+    }
 })
