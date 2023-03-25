bcb548728fc741a5e62982694e89197758ff3489
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-03-26 01:55:17 +0500

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,6 +18,6 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
