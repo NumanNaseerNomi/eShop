41fdda3f69e9ba6c527f80450f4e294785d878df
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-02 02:08:46 +0500

    Update AdminSeeder.php

--- backend/database/seeders/AdminSeeder.php
+++ backend/database/seeders/AdminSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AdminSeeder extends Seeder
 {
