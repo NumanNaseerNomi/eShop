8dd31db6f542d7f974fabd3bcef0a779e52ab000
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 04:36:54 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -28,7 +28,7 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save</button>
+            <button type="submit" class="btn btn-primary">Save</button>
           </div>
         </div>
       </div>
