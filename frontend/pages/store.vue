db74b55fa5a3b2a9e1136863008b23b49e1b83a6
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 03:11:13 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -4,6 +4,35 @@
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
       <button class="btn btn-success" type="button">Add Item</button>
     </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
     <!-- <template v-if="isLoading">
       <template v-for="n in 2">
         <div class="card mb-3">
