3703125005407d5f21065111dde0a5af48bbc535
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 03:46:41 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -38,8 +38,12 @@
         <tr>
           <th scope="row">1</th>
           <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTiKIGiH9HTFluylskypHMDTStOHsXMTzHDA&usqp=CAU" class="img-thumbnail" alt="Thumbnail" style="max-width: 4rem;"></td>
-          <td>Otto</td>
-          <td>@mdo</td>
+          <td>Shirt</td>
+          <td>Black Shirt</td>
+          <td>PKR 1000</td>
+          <td>10%</td>
+          <td>Active</td>
+          <td>Options</td>
         </tr>
       </tbody>
     </table>
