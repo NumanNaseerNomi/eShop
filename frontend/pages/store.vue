31a39497daf2647824579f83938397221a7d596c
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 22:48:43 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -11,7 +11,7 @@
             <h1 class="modal-title fs-5" id="exampleModalLabel">Details</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <form>
+          <form @submit.prevent="saveItem()">
             <div class="modal-body">
               <div class="row g-3">
                 <div class="col-md-6">
@@ -128,7 +128,7 @@
         );
       },
 
-      addToCart()
+      saveItem()
       {
         alert('addToCart()');
       }
