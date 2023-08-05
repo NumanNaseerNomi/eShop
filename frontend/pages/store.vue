a03b83daadcfb039ca6ade0d9532af68545a2511
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 17:51:23 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -57,10 +57,10 @@
     </div>
 
     <div class="d-flex justify-content-center">
-  <div class="spinner-border" role="status">
-    <span class="visually-hidden">Loading...</span>
-  </div>
-</div>
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
     <table class="table table-striped align-middle">
       <thead>
         <tr>
