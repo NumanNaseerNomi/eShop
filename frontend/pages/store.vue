4385489c37ca151c885753242d2f5d51104dd3bd
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 23:04:44 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -32,7 +32,7 @@
                 </div>
                 <div class="col-md-6">
                   <label for="inputQuantity" class="form-label">Quantity</label>
-                  <input type="number" class="form-control" id="inputQuantity" :value="currentItem?.quantity">
+                  <input type="number" class="form-control" id="inputQuantity" :value="currentItem?.quantity" required>
                 </div>
                 <div class="col-md-6">
                   <label for="formThumbnail" class="form-label">Thumbnail</label>
