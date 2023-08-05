d4fd734734233907fdea24c9da49da18602c8647
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-05 23:02:26 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -20,7 +20,7 @@
                 </div>
                 <div class="col-md-6">
                   <label for="inputName" class="form-label">Name</label>
-                  <input type="text" class="form-control" id="inputName">
+                  <input type="text" class="form-control" id="inputName" :value="currentItem?.name">
                 </div>
                 <div class="col-md-12">
                   <label for="inputDescription" class="form-label">Description</label>
