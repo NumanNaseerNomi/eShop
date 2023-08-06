f0e7b777d266a21451cf352c696e8ee572dc00b7
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-06 23:23:24 +0500

    Update store.vue

--- frontend/pages/store.vue
+++ frontend/pages/store.vue
@@ -2,7 +2,7 @@
   <div class="container my-4">
     <h1>My Store</h1>
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-      <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Item</button>
+      <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="() => { this.currentItem = {}; }">Add Item</button>
     </div>
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
