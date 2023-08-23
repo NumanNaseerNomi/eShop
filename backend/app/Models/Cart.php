2f722386432dbe7e857ef50b120662c4a010b04c
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-24 04:13:02 +0500

    Update Cart.php

--- backend/app/Models/Cart.php
+++ backend/app/Models/Cart.php
@@ -9,6 +9,8 @@ class Cart extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['product_id', 'quantity'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
