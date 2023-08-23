95ea098033e22ca5740a52071309cbd3d4318eac
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-24 02:39:13 +0500

    Update ProductsCard.vue

--- frontend/components/ProductsCard.vue
+++ frontend/components/ProductsCard.vue
@@ -38,7 +38,7 @@
         <button class="btn btn-outline-success m-4" type="button" @click="getProducts()">Load More</button>
     </div>
   </div>
-  <ProductDetailModal modalId="productDetailModal"/>
+  <ProductDetailModal modalId="productDetailModal" :product="productDetail"/>
 </template>
 <script>
   export default
