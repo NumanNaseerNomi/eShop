f88c3971ef891a6a538a7498a9ff64ec7c8126ed
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-24 01:01:47 +0500

    Update ProductsCard.vue

--- frontend/components/ProductsCard.vue
+++ frontend/components/ProductsCard.vue
@@ -38,6 +38,7 @@
         <button class="btn btn-outline-success m-4" type="button" @click="getProducts()">Load More</button>
     </div>
   </div>
+  <ProductDetailModal/>
 </template>
 <script>
   export default
