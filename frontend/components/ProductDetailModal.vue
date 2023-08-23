038e6d28d80b40359944fccc82d27188f087f976
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-24 01:18:12 +0500

    Update ProductDetailModal.vue

--- frontend/components/ProductDetailModal.vue
+++ frontend/components/ProductDetailModal.vue
@@ -1,15 +1,15 @@
 <template>
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="'#' + modalId">
       Launch demo modals
     </button>
   
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" :id="modalId" tabindex="-1" :aria-labelledby="modalId + 'Label'" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <h1 class="modal-title fs-5" :id="modalId + 'Label'">Modal title</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
@@ -26,6 +26,15 @@
 <script>
   export default
   {
+    props:
+    {
+      modalId:
+      {
+        type: String,
+        default: 'exampleModal',
+      },
+    },
+
     data()
     {
       let data =
