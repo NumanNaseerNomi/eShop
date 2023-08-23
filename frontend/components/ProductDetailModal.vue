8eebc8889251abe9652fe1a0c5e0ad7fe8c2e3fc
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-08-24 02:39:17 +0500

    Update ProductDetailModal.vue

--- frontend/components/ProductDetailModal.vue
+++ frontend/components/ProductDetailModal.vue
@@ -7,7 +7,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          {{ product.name }}
         </div>
       </div>
     </div>
