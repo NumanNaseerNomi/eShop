232953643796dba40bbaf0c45d26d70ae258a537
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-09-05 01:08:59 +0500

    Update CartController.php

--- backend/app/Http/Controllers/CartController.php
+++ backend/app/Http/Controllers/CartController.php
@@ -11,7 +11,7 @@ class CartController extends Controller
 {
     public function getCartItems(Request $request)
     {
-        $items = Cart::paginate();
+        $items = auth()->user()->cart()->paginate();
 
         return response(
             [
