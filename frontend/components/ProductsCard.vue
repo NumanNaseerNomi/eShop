7d8f6c37b933fe4e80cb849b53cb3d01025e1170
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-09-04 23:32:58 +0500

    Update ProductsCard.vue

--- frontend/components/ProductsCard.vue
+++ frontend/components/ProductsCard.vue
@@ -8,9 +8,9 @@
             <div class="card-body h6 m-0">
               <p class="card-text text-truncate">{{ product.name }}</p>
               <span class="text-success mx-2 text-nowrap">PKR {{ product.price }}</span>
-              <div class="d-grid gap-2 m-2">
+              <!-- <div class="d-grid gap-2 m-2">
                 <button class="btn btn-success" type="button" @click="addToCart(product.id)">Add to Cart</button>
-              </div>
+              </div> -->
             </div>
           </div>
         </div>
