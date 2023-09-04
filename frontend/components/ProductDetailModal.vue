9eef3532355585205ec3618d39bd6a9a8468842b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-09-05 00:00:35 +0500

    Update ProductDetailModal.vue

--- frontend/components/ProductDetailModal.vue
+++ frontend/components/ProductDetailModal.vue
@@ -32,7 +32,10 @@
               </table>
               <div class="d-grid gap-2 m-2">
                 <div class="alert alert-success" role="alert" v-show="message.length">{{ message }}</div>
-                <button class="btn btn-success" type="button" v-show="!message.length" @click="addToCart(product.id)">Add to Cart</button>
+                <button class="btn btn-success" type="button" v-show="!message.length" @click="addToCart(product.id)">
+                  <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                  Add to Cart
+                </button>
               </div>
             </div>
           </div>
