d422280a2d07d53eb7f987c094866e581bbb2f7b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-09-05 00:54:06 +0500

    Update CartController.php

--- backend/app/Http/Controllers/CartController.php
+++ backend/app/Http/Controllers/CartController.php
@@ -8,6 +8,31 @@ use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
+    public function getCartItems(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'product_id' => 'required|numeric|exists:products,id',
+                'quantity' => 'integer|min:1',
+            ]
+        );
+        
+        if($validator->fails())
+        {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+        
+        $productId = $request->input('product_id');
+        $quantity = $request->input('quantity', 1);
+        
+        $cartItem = new Cart(['product_id' => $request->product_id, 'quantity' => $quantity]);
+        
+        auth()->user()->cart()->save($cartItem);
+
+        return response()->json(['message' => 'Item added to cart successfully']);
+    }
+
     public function addToCart(Request $request)
     {
         $validator = Validator::make(
