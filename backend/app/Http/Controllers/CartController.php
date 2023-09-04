a017580f792f3e96ad717bb6abe36f23be911c94
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-09-05 00:58:11 +0500

    Update CartController.php

--- backend/app/Http/Controllers/CartController.php
+++ backend/app/Http/Controllers/CartController.php
@@ -5,32 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class CartController extends Controller
 {
     public function getCartItems(Request $request)
     {
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'product_id' => 'required|numeric|exists:products,id',
-                'quantity' => 'integer|min:1',
-            ]
-        );
-        
-        if($validator->fails())
-        {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
-        
-        $productId = $request->input('product_id');
-        $quantity = $request->input('quantity', 1);
-        
-        $cartItem = new Cart(['product_id' => $request->product_id, 'quantity' => $quantity]);
-        
-        auth()->user()->cart()->save($cartItem);
+        $items = Cart::paginate();
 
-        return response()->json(['message' => 'Item added to cart successfully']);
+        return response(
+            [
+                'status' => 'success',
+                'data' => $items,
+            ],
+            Response::HTTP_OK
+        );
     }
 
     public function addToCart(Request $request)
