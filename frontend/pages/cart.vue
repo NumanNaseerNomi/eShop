c7faefedc8231626aaf2ed429b39f4b7c6f9e40f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-09-05 00:16:02 +0500

    Update cart.vue

--- frontend/pages/cart.vue
+++ frontend/pages/cart.vue
@@ -1,50 +1,103 @@
 <template>
   <div class="container my-4">
-    <h1>My Cart Items</h1>
-    <template v-if="isLoading">
-      <template v-for="n in 2">
-        <div class="card mb-3">
-          <div class="row g-0">
-            <div class="col-md-2">
-              <svg class="w-100 h-100">
-                <rect class="w-100 h-100" fill="#adb5bd"></rect>
-              </svg>
-            </div>
-            <div class="col-md-10">
-              <div class="card-body placeholder-glow">
-                <h6 class="placeholder col-5"></h6>
-                <p>
-                  <span class="placeholder mx-2 col-3"></span>
-                  <span class="placeholder mx-2 col-3"></span>
-                  <span class="placeholder mx-2 col-3"></span>
-                </p>
-                <a href="#" tabindex="-1" class="btn btn-secondary disabled placeholder col-12"></a>
+    <h1>My Store</h1>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="() => { this.currentItem = {}; }">Add Item</button>
+    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Details</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" ref="closeButton"></button>
+          </div>
+          <form @submit.prevent="saveItem()">
+            <div class="modal-body">
+              <div class="row g-3">
+                <div class="col-md-6">
+                  <label for="inputID" class="form-label">ID</label>
+                  <input type="number" class="form-control" id="inputID" v-model="currentItem.id" disabled>
+                </div>
+                <div class="col-md-6">
+                  <label for="inputName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="inputName" v-model="currentItem.name" required>
+                </div>
+                <div class="col-md-12">
+                  <label for="inputDescription" class="form-label">Description</label>
+                  <input type="text" class="form-control" id="inputDescription" v-model="currentItem.description" required>
+                </div>
+                <div class="col-md-6">
+                  <label for="inputPrice" class="form-label">Price</label>
+                  <input type="number" class="form-control" id="inputPrice" v-model="currentItem.price" required>
+                </div>
+                <div class="col-md-6">
+                  <label for="inputQuantity" class="form-label">Quantity</label>
+                  <input type="number" class="form-control" id="inputQuantity" v-model="currentItem.quantity" required>
+                </div>
+                <div class="col-md-6">
+                  <label for="formThumbnail" class="form-label">Thumbnail</label>
+                  <input class="form-control" type="file" id="formThumbnail" ref="thumbnailInput" accept="image/*" @change="($event) => { this.currentItem.thumbnail = $event.target.files[0]; }" required>
+                </div>
+                <div class="col-md-6">
+                  <label for="inputStatus" class="form-label">Status</label>
+                  <select id="inputStatus" class="form-select" v-model="currentItem.isActive" required>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                  </select>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </template>
-    </template>
-    <template v-else>
-      <template v-for="item in items">
-        <div class="card mb-3">
-          <div class="row g-0">
-            <div class="col-md-2">
-              <NuxtLink to="/product/detail">
-                <img :src="item.url" class="img-fluid rounded-start" alt="...">
-              </NuxtLink>
+            <div class="modal-footer">
+              <button type="submit" class="btn btn-primary" :class="{'disabled': isSaving}">
+                <div class="spinner-border spinner-border-sm" role="status" v-if="isSaving">
+                  <span class="visually-hidden">Loading...</span>
+                </div>
+                <span v-else>Save</span>
+              </button>
             </div>
-            <div class="col-md-10">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
+          </form>
         </div>
-      </template>
-    </template>
+      </div>
+    </div>
+    <table class="table table-striped align-middle" v-if="items.length">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">ID</th>
+          <th scope="col">Thumbnail</th>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Price</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Status</th>
+          <th scope="col">Options</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items">
+          <th scope="row">{{ ++index }}</th>
+          <td>{{ item.id }}</td>
+          <td><img :src="getThumbnailUrl(item.thumbnail)" class="img-thumbnail" alt="Thumbnail" style="max-width: 4rem;"></td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.description }}</td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.quantity }}</td>
+          <td>{{ item.isActive ? 'Active' : 'Inactive' }}</td>
+          <td>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <button type="button" class="btn btn-primary" @click="editItem(item.id)" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
+              <button type="button" class="btn btn-danger" @click="deleteItem(item.id)">Delete</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="d-flex justify-content-center" v-if="isLoading">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    <h2 class="text-center" v-if="!items.length && !isLoading">Store is Empty</h2>
   </div>
 </template>
 <script>
@@ -54,8 +107,10 @@
     {
       let data =
       {
-        items: null,
+        items: [],
+        currentItem: {},
         isLoading: false,
+        isSaving: false,
       }
 
       return data;
@@ -63,29 +118,91 @@
 
     created()
     {
-      this.getCartItems();
+      this.getStoreItems();
     },
     
     methods:
     {
-      getCartItems()
+      getStoreItems()
       {
+        let url = useRuntimeConfig().public.API_URL + '/getProducts';
         this.isLoading = true;
 
-        fetch('https://jsonplaceholder.typicode.com/photos?_limit=2')
+        fetch(url)
         .then(response => response.json())
         .then(json =>
           {
-            this.items = json;
+            this.items = json.data.data;
             this.isLoading = false;
           }
         );
       },
 
-      addToCart()
+      saveItem()
       {
-        alert('addToCart()');
-      }
+        let url = useRuntimeConfig().public.API_URL + '/saveProduct';
+
+        let formData = new FormData();
+  
+        for(let field in this.currentItem)
+        {
+          formData.append(field, this.currentItem[field]);
+        }
+
+        let payload =
+        {
+          method: "POST",
+          headers: { 'Authorization': 'Bearer ' + localStorage.getItem('accessToken'), },
+          body: formData
+        };
+        
+        this.isSaving = true;
+        
+        fetch(url, payload)
+        .then((response) => response.json())
+        .then((data) =>
+          {
+            this.$refs.closeButton.click();
+            this.currentItem.id ?? this.items.push(data.data);
+            this.currentItem = {};
+            this.$refs.thumbnailInput.value = '';
+            this.isSaving = false;
+          }
+        )
+        .catch((error) => { console.error("Error:", error); });
+      },
+
+      editItem(id)
+      {
+        this.currentItem = this.items.find((item) => item.id === id);
+      },
+
+      deleteItem(id)
+      {
+        let url = useRuntimeConfig().public.API_URL + '/deleteProduct';
+        let payload =
+        {
+          method: "DELETE",
+          headers:
+          {
+            "Content-Type": "application/json",
+            'Authorization': 'Bearer ' + localStorage.getItem('accessToken'),
+          },
+          body: JSON.stringify({ id: id })
+        };
+
+        this.items = this.items.filter(item => item.id !== id);
+
+        fetch(url, payload)
+        .then((response) => response.json())
+        .then((data) => {})
+        .catch((error) => { console.error("Error:", error); });
+      },
+
+      getThumbnailUrl(thumbnail)
+      {
+        return useRuntimeConfig().public.API_URL.replace(/\/api$/, '') + '/storage/' + thumbnail;
+      },
     }
   }
 </script>
