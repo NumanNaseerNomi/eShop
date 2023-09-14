b13fddff5f6989afdddf301fcdf4a87bb9711880
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2023-09-14 19:21:28 +0500

    Revert "Update index.vue"
    This reverts commit f78ee4937cfbd9a299d1df15262bd67804b0bf74.

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,3 @@
 <template>
   <ProductsCard/>
-<div class="container my-4 p-5">
-  <h2 class="text-center" v-if="!items.length && !isLoading">Store is Empty</h2>
-</div>
 </template>
-<script>
-    export default
-    {
-        data()
-        {
-            let data =
-            {
-                items: [],
-                isLoading: false,
-            }
-
-            return data;
-        },
-    }
-</script>
